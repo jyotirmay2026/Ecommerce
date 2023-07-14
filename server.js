7de131266b42bacf24e4613c684258fc643bc43d
jyotirmay2026 <jyotirmaysharma02@gmail.com>
2023-07-14 18:17:15 +0530

    chenges on package.json file

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ import productRoutes from "./routes/productRoutes.js";
 import cors from "cors";
 import path from 'path';
 import { fileURLToPath } from "url";
-import { Z_FIXED } from "zlib";
+
 //configure env
 dotenv.config();
 
